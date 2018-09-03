bbb74bbdb91fcc7a801f0b2d9928bad455a2165e
Eric <yg0803804@otc.edu>
2018-09-03 18:32:04 -0500

    Create the CIS131JSIntro file

---
+++ CIS131JSIntro.js
@@ -0,0 +1,17 @@
+"use strict"
+/*
+    CIS-131-W01
+    Yunze (Eric) Guo
+    Sep 3, 2018
+
+    This JS file to convert temprature units, which
+    asks for a temperature in Celsius degrees, then
+    displays it in Fahrenheit degrees.
+
+*/
+
+//declear the variables
+let celsius = 32;
+let fahrenheit = celsius * 9 / 5 + 32;
+//display the result
+console.log("The " + celsius + " Celsius equal to " + fahrenheit + " Fahrenheit.");
